3ecbcb230e3b797d621942c47852baaee33f6a2f
martin sahadad <martin.sahadad@elev.ga.ntig.se>
2024-11-29 10:02:24 +0100

    Uppgift 3

--- Steg 2/Program.cs
+++ Steg 2/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿  
+ // Console.WriteLine("Här är en lista på 5 leksaker");
+  
+  //string[] leksaker = { "Teddy björn", "Lego", "bilar", "Rubik's kub", "Glock 18" };
+
+  //Console.ReadLine(); 
+
+     // Console.WriteLine("Här är en lista på 5 leksaker:");
+
+       // string[] leksaker = { "Teddy björn", "Lego", "Bilar", "Rubik's kub", "Glock 18" };
+
+      //  foreach (string leksak in leksaker)
+        //{
+          //  Console.WriteLine(leksak);
+        //}
+
+        //Console.ReadLine();
+
+    
+
+    Console.WriteLine("Här är namn på 5 personer!");
+
+
+    string[] namn = { "Martin", "Janan", "Rasmus", "Imad", "Adam"}; 
+
+    foreach (string person in namn) 
+    {
+       Console.WriteLine(person); 
+    }
+
+    Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
